2f90d4fc9d0664f5387da5c36ab07a5123bcb99b
Feiran <feiranz116@gmail.com>
2025-03-21 18:33:25 -0400

    Added binomial random numbers and updated report

--- code/01_make_output.R
+++ code/01_make_output.R
@@ -25,3 +25,11 @@ saveRDS(
   random_numbers3,
   file = here::here("output", "random_numbers3.rds")
 )
+
+set.seed(4)
+random_numbers4 <- rbinom(100, 1, 0.25)
+
+saveRDS(
+  random_numbers4,
+  file = here::here("output", "random_numbers4.rds")
+)
